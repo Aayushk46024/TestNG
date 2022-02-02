fe331dd85467725ef8a3c84f9f2ddd9bd1b597d4
AayushKhandelwal <aayush.khandelwal@infostride.com>
2022-02-02 18:03:55 +0530

    Assignment of groups in groups

--- src/TestNG/Groups_in_Groups.java
+++ src/TestNG/Groups_in_Groups.java
@@ -6,16 +6,16 @@ public class Groups_in_Groups {
         @Test(groups= {"Smoke"})
         public void Atest1()
         {
-            System.out.println("test1");
+            System.out.println("test1 completed");
         }
         @Test(groups= {"Regression"})
         public void Btest2()
         {
-            System.out.println("test2");
+            System.out.println("test2 completed");
         }
         @Test(groups= {"Sanity"})
         public void Atest3()
         {
-            System.out.println("test3");
+            System.out.println("test3 completed");
         }
 }
